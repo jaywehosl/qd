bd7e38edc88b1b67a04d0199d088834f54223be6
qd <qd@local>
2026-09-23 21:31:39 +0300

    qd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ru.quicdiver.client"
         minSdk = 33
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.0.8"
+        versionCode = 10
+        versionName = "0.0.9"
         ndk {
             abiFilters += "arm64-v8a"
         }
